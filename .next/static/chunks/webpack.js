a149fc304473de69425db96b6dadc8d6942a4406
CHKMTY <matheus.wesleys@hotmail.com>
2021-01-31 23:07:33 -0300

    Changed 'projectUrl' in index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9652f0c6406f4f5300c3";
+/******/ 	var hotCurrentHash = "92fc40e932daae251259";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
