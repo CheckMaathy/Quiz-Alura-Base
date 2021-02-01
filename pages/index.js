5eb802463052b88e13b6c481e59cf65755517954
CHKMTY <matheus.wesleys@hotmail.com>
2021-02-01 00:04:14 -0300

    Changed image background

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
                 value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-                {`Jogar ${name}`}
+                {`Jogar como ${name}`}
               </Button>
             </form>
           </Widget.Content>
--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -4,6 +4,7 @@ const QuizBackground = styled.div`
   width: 100%;
   background-size: cover;
   background-position: center;
+  //background-image: '../src/components/QuizBackground/11914.jpg'
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-color: ${({ theme }) => theme.colors.mainBg};
   flex: 1;
