0f7013a756cf670ddc0ee02b64c7dda6df23e924
CHKMTY <matheus.wesleys@hotmail.com>
2021-01-31 23:02:17 -0300

    update files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b3e1aaea3c993552f736";
+/******/ 	var hotCurrentHash = "9652f0c6406f4f5300c3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
