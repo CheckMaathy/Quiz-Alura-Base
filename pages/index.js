5c64e95e9dfd5bc45694af84059acb50bde10d67
CHKMTY <matheus.wesleys@hotmail.com>
2021-01-31 22:36:19 -0300

    New Files

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import Head from 'next/head';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizBackground from '../src/components/QuizBackground';
-import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner';
+import styled from 'styled-components'
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px){
-    margin: auto;
-    padding: 15px;
-
-  }
-`;
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`
 
 export default function Home() {
-  return (
-    <QuizBackground backgroundImage={db.bg}>
-      <Head>
-        <tittle>Alura Quiz - Modelo Base</tittle>
-      </Head>
-      <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1> Pokémon </h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>Loren ipson dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-
-        <Widget>
-          <Widget.Header>
-            <h1> Quizes da Galera </h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>Loren ipson dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/CheckMaathy" />
-    </QuizBackground>
-  );
+  return <Title>My page</Title>
 }
