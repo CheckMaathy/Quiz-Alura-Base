caebb33e569afa77d4f4373b6a6857956fc977c2
CHKMTY <matheus.wesleys@hotmail.com>
2021-01-31 21:48:33 -0300

    Update index.js Archives

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -21,34 +21,31 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-  <QuizBackground backgroundImage={db.bg}>
-    <Head>
-      <tittle>Alura Quiz - Modelo Base</tittle>
-    </Head>
-    <QuizContainer>
-      <Widget>
-        <Widget.Header>
-          <h1> Pokémon </h1>
-        </Widget.Header>
-          <Widget.Content>            
-
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <tittle>Alura Quiz - Modelo Base</tittle>
+      </Head>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1> Pokémon </h1>
+          </Widget.Header>
+          <Widget.Content>
             <p>Loren ipson dolor sit amet...</p>
           </Widget.Content>
-      </Widget>
+        </Widget>
 
-      <Widget>
-        <Widget.Header>
-          <h1> Quizes da Galera </h1>
-        </Widget.Header>
-        <Widget.Content>
-
-          <p>Loren ipson dolor sit amet...</p> 
-        </Widget.Content>               
-      </Widget>
-      <Footer />
-    </QuizContainer>
-    <GitHubCorner projectUrl="https://github.com/CheckMaathy" />
-  </QuizBackground>
-  
+        <Widget>
+          <Widget.Header>
+            <h1> Quizes da Galera </h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Loren ipson dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/CheckMaathy" />
+    </QuizBackground>
   );
 }
